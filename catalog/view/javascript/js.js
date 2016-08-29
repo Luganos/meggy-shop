5ebf8e8f8ac0a4604ec9eeff612832d50c409860
tocea1984 <tocea@yandex.ru>
2016-08-29 10:50:33 +0300

    29_08_2

--- catalog/view/javascript/js.js
+++ catalog/view/javascript/js.js
@@ -28,8 +28,8 @@ $('.sl').slick({
 $('.sl_slide_1').on("mouseover", function(){$(".sl_kor_1").css({"visibility": "visible"});});
 $('.sl_slide_1').on("mouseout", function(){$(".sl_kor_1").css({"visibility": "hidden"});});
 
-$('.sl_img_1').on("mouseover", function(){$(".sl_img_1").css({"background-size": "180%"});});
-$('.sl_img_1').on("mouseout", function(){$(".sl_img_1").css({"background-size": "90%"});});
+$('.sl_img_1').on("mouseover", function(){$(".sl_img_new").css({"width": "120%"});});
+$('.sl_img_1').on("mouseout", function(){$(".sl_img_new").css({"width": "80%"});});
 
 
 $('.sl_slide_2').on("mouseover", function(){$(".sl_kor_2").css({"visibility": "visible"});});
@@ -50,3 +50,8 @@ $('.sl_slide_4').on("mouseout", function(){$(".sl_kor_4").css({"visibility": "hi
 $('.sl_img_4').on("mouseover", function(){$(".sl_img_4").css({"background-size": "180%"});});
 $('.sl_img_4').on("mouseout", function(){$(".sl_img_4").css({"background-size": "90%"});});
 
+$('.sl_slide_5').on("mouseover", function(){$(".sl_kor_5").css({"visibility": "visible"});});
+$('.sl_slide_5').on("mouseout", function(){$(".sl_kor_5").css({"visibility": "hidden"});});
+
+$('.sl_img_5').on("mouseover", function(){$(".sl_img_5").css({"background-size": "180%"});});
+$('.sl_img_5').on("mouseout", function(){$(".sl_img_5").css({"background-size": "90%"});});
