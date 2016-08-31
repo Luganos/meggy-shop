a7718df8368f07f10a1a4e85b03a1f7aa509bd1b
Merecoder <ponomarev.link@gmail.com>
2016-08-31 11:43:13 +0300

    Change module seasonproposal.php

--- admin/language/english/module/seasonproposal.php
+++ admin/language/english/module/seasonproposal.php
@@ -1,18 +1,22 @@
 <?php
 // Heading
-$_['heading_title']       = 'Season Proposal';
+$_['heading_title']    = 'Season Proposal';
 
 // Text
-$_['text_module']         = 'Modules';
-$_['text_success']        = 'Success: You have modified module "Season Proposal"!';
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified Season Proposal module!';
 $_['text_edit']        = 'Edit Season Proposal Module';
 
 // Entry
 $_['entry_name']       = 'Module Name';
-$_['entry_limit']      = 'Limit';
+$_['entry_banner']     = 'Banner';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
 $_['entry_status']     = 'Status';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify module "Season proposal"!';
+$_['error_permission'] = 'Warning: You do not have permission to modify Season Proposal module!';
 $_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
 
