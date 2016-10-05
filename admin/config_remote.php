00de74b9cde4bc9c3ac3c97d389ce4b0bdf52e66
Merecoder <ponomarev.link@gmail.com>
2016-10-05 17:04:19 +0300

    merge 1 05.10.2016

---
+++ admin/config_remote.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://meggi.geeker.pro/admin/');
+define('HTTP_CATALOG', 'http://meggi.geeker.pro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://meggi.geeker.pro/admin/');
+define('HTTPS_CATALOG', 'http://meggi.geeker.pro/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/meggi/data/www/meggi.geeker.pro/admin/');
+define('DIR_SYSTEM', '/var/www/meggi/data/www/meggi.geeker.pro/system/');
+define('DIR_LANGUAGE', '/var/www/meggi/data/www/meggi.geeker.pro/admin/language/');
+define('DIR_TEMPLATE', '/var/www/meggi/data/www/meggi.geeker.pro/admin/view/template/');
+define('DIR_CONFIG', '/var/www/meggi/data/www/meggi.geeker.pro/system/config/');
+define('DIR_IMAGE', '/var/www/meggi/data/www/meggi.geeker.pro/image/');
+define('DIR_CACHE', '/var/www/meggi/data/www/meggi.geeker.pro/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/meggi/data/www/meggi.geeker.pro/system/storage/download/');
+define('DIR_LOGS', '/var/www/meggi/data/www/meggi.geeker.pro/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/meggi/data/www/meggi.geeker.pro/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/meggi/data/www/meggi.geeker.pro/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/meggi/data/www/meggi.geeker.pro/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'meggy');
+define('DB_PASSWORD', '71LMrJ22');
+define('DB_DATABASE', 'meggy');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'rl_');
