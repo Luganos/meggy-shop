46c430b66c9fc01b57c9eff756a065c4324950c5
Merecoder <ponomarev.link@gmail.com>
2016-10-10 11:31:52 +0300

    fixed bug of get class for certain id

--- admin/view/javascript/bind-option/bind_option.js
+++ admin/view/javascript/bind-option/bind_option.js
@@ -7,33 +7,42 @@ $('body').on('click',function(e){
 
   tr = e.target.id;
   tr = '#'+tr;
-
-    trr = e.target.id;
-
-$(tr).autocomplete({
-  'source': function(request, response) {
-    $.ajax({
-      url: 'index.php?route=catalog/product/autocomplete&token='+token+'&filter_sku=' +  encodeURIComponent(request),
-      dataType: 'json',
-      success: function(json) {
-        json.unshift({
-          sku: 0,
-          name: 'Нет связей'
+  
+  var class_name = null;
+  class_name = $(tr).attr('class');
+  
+  trr = e.target.id;
+ 
+  if (class_name !== undefined) {
+      
+     if(class_name.match( /bind/i )) {
+         
+       $(tr).autocomplete({
+           'source': function(request, response) {
+            $.ajax({
+                url: 'index.php?route=catalog/product/autocomplete&token='+token+'&filter_sku=' +  encodeURIComponent(request),
+                dataType: 'json',
+                success: function(json) {
+                    json.unshift({
+                      sku: 0,
+                      name: 'Нет связей'
+                    });
+                    response($.map(json, function(item) {
+                        return {
+                             label: item['sku'],
+                             value: item['sku']
+                        }
+                    }));
+                }
+            });
+          },
+          'select': function(item) {
+                       document.getElementById(trr).setAttribute('value',item['label']);
+                       var name = $('#' + trr).attr('name');
+                       $('input[name=\''+name+'\']').val(item['value']);
+           }
         });
-        response($.map(json, function(item) {
-          return {
-            label: item['sku'],
-            value: item['sku']
-          }
-        }));
-      }
-    });
-  },
-  'select': function(item) {
-   document.getElementById(trr).setAttribute('value',item['label']);
-   var name = $('#' + trr).attr('name');
-   $('input[name=\''+name+'\']').val(item['value']);
-  }
-});
-});
+       }
+     }
+  });
 });
